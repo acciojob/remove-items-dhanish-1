d70fbd85452cac1e1867dd9c1a7b6b51503868eb
Dhanish kumar <dhanishmca1@gmail.com>
2025-02-14 12:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,22 @@
 // add event listener to input
 // then find the selected option using selectedIndex
 // remove it.
+// document.querySelector("input[type='button']").addEventListener("click", () => {
+//     let colorSelect = document.getElementById("colorSelect");
+//     let selectedOption = colorSelect.options[colorSelect.selectedIndex];
+//     if (selectedOption) {
+//         selectedOption.remove();
+//     }
+// });
+
 document.querySelector("input[type='button']").addEventListener("click", () => {
+    // Get the dropdown element
     let colorSelect = document.getElementById("colorSelect");
+    
+    // Get the currently selected option
     let selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    
+    // Remove the selected option if it exists
     if (selectedOption) {
         selectedOption.remove();
     }
